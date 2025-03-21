596cf83489f517762905e53bdae3b98844680f08
phumiphas-nopthaisong <pumipas.n@gmail.com>
2025-03-21 13:45:23 +0700

    fix: update annotation for controller

--- src/main/java/com/test/testapi/api/HelloApi.java
+++ src/main/java/com/test/testapi/api/HelloApi.java
@@ -2,9 +2,9 @@ package com.test.testapi.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController("/api/v1")
+@RequestMapping("/api/v1")
 public interface HelloApi {
 
     @GetMapping("/hello/{name}")
--- src/main/java/com/test/testapi/controller/HelloController.java
+++ src/main/java/com/test/testapi/controller/HelloController.java
@@ -2,8 +2,10 @@ package com.test.testapi.controller;
 
 import com.test.testapi.api.HelloApi;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@RestController
 public class HelloController implements HelloApi {
 
     @Override
